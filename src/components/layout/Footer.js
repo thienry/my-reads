ef6953993766a23f8f845bdee72a9779b6e861c5
Thiago Moura <thmoura14@hotmail.com>
2019-08-11 17:42:56 -0300

    Removed madeWithlove in footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,6 +1,5 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { FaHeart } from "react-icons/fa";
 
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Typography from "@material-ui/core/Typography";
@@ -10,9 +9,8 @@ import Link from "@material-ui/core/Link";
 function MadeWithLove({ name }) {
   return (
     <Typography variant="body1" align="center" color="inherit">
-      {"Feito com "} <FaHeart /> {"| Desenvolvido por"}
+      {"Desenvolvido por "}
       <Link color="inherit" href="https://thiagotec.com/">
-        {" "}
         {name}
       </Link>
     </Typography>
@@ -35,7 +33,7 @@ const Footer = () => {
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
-    flexDirection: "column",
+    flexDirection: "column"
   },
   footer: {
     padding: theme.spacing(3),
